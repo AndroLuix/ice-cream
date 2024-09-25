445a7086c7f372f9067c560e302b21817d0dd5bf
luigi iadicola <luigiadicola@gmail.com>
2024-09-25 17:24:33 +0200

    button

--- src/components/IceCreamComponents.js
+++ src/components/IceCreamComponents.js
@@ -11,14 +11,12 @@ const IceCreamComponents = () => {
       <section className=' section-center'>
         <h3>Nice Cream</h3>
         <Menu />
+        <a className="btn" style={{background:'white'}} target='_blank' href='https://iadicola.netsons.org/contatti'>
+          Crea il tuo menu online
+        </a>
       </section>
 
-      <section className='section-center'>
-        <button className="btn">
-          Add Your IceCream
-        </button>
-      </section>
-
+      
     </div>
   )
 }
